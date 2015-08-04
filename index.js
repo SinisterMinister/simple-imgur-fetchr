4a44518211dda3a64b97c3f64214ab1b2271e2c2
Codey Whitt <codey.whitt@pearson.com>
2015-08-04 12:12:15 -0600

    Refactored to be a bit cleaner

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 var yargs = require('yargs'),
-	imgur = require('./lib/imgur'),
-	_ = require("lodash"),
-	Promise = require("bluebird"),
-	fs = require("fs"),
-	path = require("path");
+	path = require("path"),
+	core = require("./lib/core");
 
 // Setup Yargs
-yargs.usage("Usage: $0 <subreddit> <location> [options]")
+var argv = yargs.usage("Usage: $0 <subreddit> <location> [options]")
 	.demand(2)
 	.wrap(yargs.terminalWidth())
 	.options({
@@ -26,12 +23,7 @@ yargs.usage("Usage: $0 <subreddit> <location> [options]")
 			default: 5,
 			describe: "Number of workers to download concurrently."
 		}
-	});
-
-var argv = yargs.argv;
-
-// Promisify FS
-Promise.promisifyAll(fs);
+	}).argv;
 
 var subreddit = argv._[0],
 	pages = argv.pages,
@@ -40,93 +32,10 @@ var subreddit = argv._[0],
 	location = path.resolve(path.normalize(argv._[1])),
 	ids = [];
 
-// Verify the download location is legit
-try {
-	var stats = fs.lstatSync(location);
-
-	if ( ! stats.isDirectory()) {
-		console.error("Download location must be a directory");
-		return process.exit(1);
-	}
-}
-catch (e) {
-	console.error(e);
-	return process.exit(1);
-}
-
-// Get a list of file IDs to prevent duplicate downloads
-fs.readdirAsync(location).then(function (files) {
-	var stats = {};
-
-	// Load the stats of the files
-	for (var i = files.length - 1; i >= 0; i--) {
-		ids.push(path.parse(path.join(location, files[i])).name);
-	}
-
-	return imgur.getTopImagesFromSubreddit(subreddit, pages, timeframe);
-})
-
-.then(function (data) {
-	// TODO: Only download the new stuff
-	var queue = [], skipped = 0;
-
-	console.info("Found " + data.length + " images.");
-
-	for (var i = data.length - 1; i >= 0; i--) {
-		// Skip low res images
-		if (data[i].width < 1920 || data[i].height < 1080) {
-			console.info("Skipping low res image " + data[i].id);
-			skipped++;
-			continue;
-		}
-
-		// Skip portrait image
-		if (data[i].width < data[i].height) {
-			console.info("Skipping portrait image " + data[i].id);
-			skipped++;
-			continue;
-		}
-
-		// Skip duplicates
-		if (_.includes(ids, data[i].id)) {
-			console.info("Skipping duplicate image " + data[i].id);
-			skipped++;
-			continue;
-		}
-
-		queue.push(data[i].id);
-	}
-
-	queue = _.uniq(queue);
-	console.info("Skipped " + skipped + " images");
-	console.info("Fetching " + queue.length + " images.");
-	
-	return processQueue(queue, workers);
-})
-
-.then(function (data) {
-	console.info("Done.");
-})
-.catch(function (err) {
-	console.error(err);
-	return process.exit(1);
+core.fetchImages(subreddit, location, pages, timeframe, workers).then(function () {
+	console.info("Download complete!");
+	process.exit();
+}).catch(function (err) {
+	console.info(err);
+	process.exit(1);
 });
-
-function processQueue(queue, workers) {
-	var processImage = function () {
-		return new Promise(function (resolve, reject) {
-			if (queue.length > 0)
-				imgur.downloadImage(queue.shift(), location).then(function () {
-					processImage().then(resolve, reject);
-				}).catch(reject);
-			else
-				resolve();
-		});
-	};
-
-	return new Promise(function (resolve, reject) {
-		for (var i = workers - 1; i >= 0; i--) {
-			processImage().then(resolve, reject);
-		}
-	});
-}
